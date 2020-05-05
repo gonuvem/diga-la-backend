5b0aa8e69f23d36d45b231917c97b4fcbb4c3e45
Edson Moita <moitaneto@hotmail.com>
2020-05-05 11:04:01 -0300

    Refatora testes de exemplo.

--- src/__tests__/example.test.ts
+++ src/__tests__/example.test.ts
@@ -2,29 +2,30 @@ import request from 'supertest'
 
 import app from '../app'
 
-describe('Example Test', () => {
-  /** Testar endpoint raiz */
-  test('200 Server up', () => {
-    return request(app)
-      .get('/')
-      .then((response) => {
-        expect(response.status).toBe(200)
-        expect(response.body).toMatchObject({
-          message: 'DIG API'
-        })
+const testRestRoot = (): Promise<void> => {
+  return request(app)
+    .get('/')
+    .then((response) => {
+      expect(response.status).toBe(200)
+      expect(response.body).toMatchObject({
+        message: 'DIG API'
       })
-  })
-})
+    })
+}
 
-describe('GQL Example Test', () => {
-  /** Testar endpoint raiz */
-  test('200 Server up', () => {
-    return request(app)
-      .post('/graphql')
-      .send({ query: 'query { wakeUp }' })
-      .then((response) => {
-        expect(response.status).toBe(200)
-        expect(response.body.data.wakeUp).toBe('DIG API')
-      })
-  })
+const testGqlWakeUp = (): Promise<void> => {
+  return request(app)
+    .post('/graphql')
+    .send({ query: 'query { wakeUp }' })
+    .then((response) => {
+      expect(response.status).toBe(200)
+      expect(response.body.data.wakeUp).toBe('DIG API')
+    })
+}
+
+/** Testes de exemplo */
+describe('Example Tests', () => {
+  test('200 Server up - REST', testRestRoot)
+
+  test('200 Server up - GRAPHQL', testGqlWakeUp)
 })
