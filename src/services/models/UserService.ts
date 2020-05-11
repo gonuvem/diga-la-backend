5e29757f5bc9e0e963695348798ce64acd5634ee
Edson Moita <moitaneto@hotmail.com>
2020-05-11 10:11:28 -0300

    Atualiza serviço de usuário com as demais funções do CRUD.

--- src/services/models/UserService.ts
+++ src/services/models/UserService.ts
@@ -1,5 +1,19 @@
-import User from '../../models/User'
-import { USER_NOT_FOUND } from '../../middlewares/errorHandling/errors'
-import { fetchOne } from '../../utils/mongoose'
+import User from 'src/models/User'
+import { USER_NOT_FOUND } from 'src/middlewares/errorHandling/errors'
+import {
+  fetchOne,
+  updateOne,
+  deleteOne,
+  createOneObject,
+  createOne
+} from 'src/utils/mongoose'
 
 export const fetchOneUser = fetchOne(User, USER_NOT_FOUND)
+
+export const updateOneUser = updateOne(User, USER_NOT_FOUND)
+
+export const deleteOneUser = deleteOne(User, USER_NOT_FOUND)
+
+export const createOneUserObject = createOneObject(User)
+
+export const createOneUser = createOne(User)
