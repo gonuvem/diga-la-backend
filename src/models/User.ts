42b5688f3d51b3556983b66b0ac9cb93e0eccc10
Edson Moita <moitaneto@hotmail.com>
2020-05-14 12:06:16 -0300

    Cria o middleware que faz a hash de senha do usuário.

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
 
 import { UserDocument } from '../interfaces'
 import { Role } from '../enums'
+
+import { SALT_WORK_FACTOR } from '../utils/constants'
 
 const UserSchema = new mongoose.Schema({
   /** Nome completo */
@@ -30,6 +33,22 @@ const UserSchema = new mongoose.Schema({
     default: ''
   }
 }, { timestamps: true })
+
+/**
+ * Password hash middleware.
+ */
+UserSchema.pre('save', function save (next) {
+  const user = this as UserDocument
+  if (!user.isModified('password')) { return next() }
+  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+    if (err) { return next(err) }
+    bcrypt.hash(user.password, salt, function (err: mongoose.Error, hash) {
+      if (err) { return next(err) }
+      user.password = hash
+      next()
+    })
+  })
+})
 
 const User = mongoose.model<UserDocument>('User', UserSchema)
 
