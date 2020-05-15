34b266b28fe4d69154306666b284230e0916c662
Edson Moita <moitaneto@hotmail.com>
2020-05-15 16:40:34 -0300

    Coloca tipo nos parâmetros do login para seguir a interface implementada.

--- src/graphql/resolvers/AuthResolvers.ts
+++ src/graphql/resolvers/AuthResolvers.ts
@@ -1,9 +1,9 @@
 import { wrapGqlAsyncFunc } from '../../middlewares/errorHandling/errorHelper'
 import validateGqlRequest from '../../middlewares/validation'
-import { MyContext, LoginResponse } from '../../types'
+import { MyContext, LoginParams, LoginResponse } from '../../types'
 import * as AuthResolverHelper from '../../helpers/resolverHelpers/AuthResolverHelper'
 
-const login = (_parent: object, _args: object, context: MyContext)
+const login = (_parent: object, _args: object, context: MyContext<LoginParams>)
 : Promise<LoginResponse> => {
   return AuthResolverHelper.login(context.validData)
 }
