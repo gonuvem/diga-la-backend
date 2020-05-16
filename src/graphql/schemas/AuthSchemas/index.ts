7bd78437e7b01723c4bd939578f648e495bef19f
Edson Moita <moitaneto@hotmail.com>
2020-05-16 10:40:28 -0300

    Cria resolver e schemas graphql para mutation validateToken.

--- src/graphql/schemas/AuthSchemas/index.ts
+++ src/graphql/schemas/AuthSchemas/index.ts
@@ -1,9 +1,12 @@
 import * as loginSchemas from './loginSchemas'
+import * as validateTokenSchemas from './validateTokenSchemas'
 
 export const types = `
 ${loginSchemas.types}
+${validateTokenSchemas.types}
 `
 
 export const Mutation = `
 ${loginSchemas.Mutation}
+${validateTokenSchemas.Mutation}
 `
