04a838c6b5a743d628b0729212569227fd19751d
Edson Moita <moitaneto@hotmail.com>
2020-05-16 15:45:39 -0300

    Autoriza papel 'client' a acessar query readOwnProfile.

--- src/middlewares/authorization/authorizationRouter.ts
+++ src/middlewares/authorization/authorizationRouter.ts
@@ -4,4 +4,6 @@ import { Role } from '../../enums'
  * Determina os papéis que podem acessar uma operação grapqhl
  * @example { createUser: [Role.Dev, Role.Admin] }
  */
-export const gqlRouter: { [resolverName: string]: Role[] } = {}
+export const gqlRouter: { [resolverName: string]: Role[] } = {
+  readOwnProfile: [Role.Client]
+}
