74b7a3955385933ceff1657895dd4b8e946b1b8a
Edson Moita <moitaneto@hotmail.com>
2020-05-16 12:22:39 -0300

    Cria testes para mutation forgotPassword.

--- src/__tests__/resolvers/AuthResolvers/AuthResolvers.test.ts
+++ src/__tests__/resolvers/AuthResolvers/AuthResolvers.test.ts
@@ -1,8 +1,11 @@
 import loginTest from './loginTest'
 import validateTokenTest from './validateTokenTest'
+import forgotPasswordTest from './forgotPasswordTest'
 
 describe('Test Auth Resolvers', () => {
   describe('Test login', loginTest)
 
   describe('Test validateToken', validateTokenTest)
+
+  describe('Test forgotPassword', forgotPasswordTest)
 })
--- src/__tests__/resolvers/AuthResolvers/forgotPasswordTest.ts
+++ src/__tests__/resolvers/AuthResolvers/forgotPasswordTest.ts
@@ -1,1 +1,49 @@
-export default (): void => {}
+/* eslint-disable max-lines-per-function */
+import app from '../../../app'
+import * as utils from '../../utils'
+import * as helpers from '../../helpers'
+import * as err from '../../../middlewares/errorHandling/errors'
+import * as gqlFieldsQuery from '../../gqlFieldsQuery'
+import { ForgotPasswordParams } from '../../../types'
+
+const resolver = 'forgotPassword'
+
+// eslint-disable-next-line max-lines-per-function
+const createQuery = ({ email }: ForgotPasswordParams): string => `
+mutation {
+  ${resolver}(email: "${email}") {
+    error ${gqlFieldsQuery.errorFieldsQuery}
+  }
+}
+`
+
+const baseRequest = utils.baseGqlRequest(app, createQuery)
+
+export default (): void => {
+  test('404 User not found', () => {
+    return baseRequest({ email: 'email@email.com' })
+      .then(utils.expectGqlError(err.USER_NOT_FOUND, resolver))
+  })
+
+  test.skip('200 Email sent', async () => {
+    // Modifique essas variáveis em jest.config.js antes de testar
+    // process.env.SG_API_KEY = ''
+    // process.env.CONTACT_EMAIL = ''
+
+    // Crie um email temporário em https://www.tempmailaddress.com/
+    const email = 'zeke.abshir@andyes.net'
+
+    const user = await helpers.createUser({ body: { email } })
+
+    return baseRequest({ email: user.email })
+      .then(response => {
+        utils.printForDocs(response.body)
+        const { error } = response.body.data[resolver]
+        expect(error).toBeNull()
+      })
+  }, 10000)
+
+  afterAll(async () => {
+    await helpers.dropCollection({ modelName: 'User' })
+  })
+}
