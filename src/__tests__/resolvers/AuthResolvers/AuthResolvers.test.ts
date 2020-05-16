cfb81b88a7a65b447395844401457b95ef97f085
Edson Moita <moitaneto@hotmail.com>
2020-05-16 10:39:39 -0300

    Cria testes para mutation validateToken.

--- src/__tests__/resolvers/AuthResolvers/AuthResolvers.test.ts
+++ src/__tests__/resolvers/AuthResolvers/AuthResolvers.test.ts
@@ -1,5 +1,8 @@
 import loginTest from './loginTest'
+import validateTokenTest from './validateTokenTest'
 
 describe('Test Auth Resolvers', () => {
   describe('Test login', loginTest)
+
+  describe('Test validateToken', validateTokenTest)
 })
