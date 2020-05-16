0ca817cfb2fe63164721c4f5253311ce44f39145
Edson Moita <moitaneto@hotmail.com>
2020-05-16 12:24:08 -0300

    Cria schemas e resolvers graphql para mutation forgotPassword.

--- src/graphql/resolvers/AuthResolvers.ts
+++ src/graphql/resolvers/AuthResolvers.ts
@@ -5,7 +5,8 @@ import {
   MyContext,
   LoginParams,
   LoginResponse,
-  ValidateTokenParams
+  ValidateTokenParams,
+  ForgotPasswordParams
 } from '../../types'
 
 const login = (_parent: object, _args: object, context: MyContext<LoginParams>)
@@ -19,7 +20,14 @@ const validateToken = (_parent: object, _args: object,
   return AuthResolverHelper.validateToken(context.validData)
 }
 
+const forgotPassword = (_parent: object, _args: object,
+  context: MyContext<ForgotPasswordParams>)
+: Promise<{}> => {
+  return AuthResolverHelper.forgotPassword(context.validData)
+}
+
 export const Mutation = {
   login: wrapGqlAsyncFunc(validateGqlRequest(login)),
-  validateToken: wrapGqlAsyncFunc(validateGqlRequest(validateToken))
+  validateToken: wrapGqlAsyncFunc(validateGqlRequest(validateToken)),
+  forgotPassword: wrapGqlAsyncFunc(validateGqlRequest(forgotPassword))
 }
--- src/graphql/schemas/AuthSchemas/index.ts
+++ src/graphql/schemas/AuthSchemas/index.ts
@@ -1,12 +1,15 @@
 import * as loginSchemas from './loginSchemas'
 import * as validateTokenSchemas from './validateTokenSchemas'
+import * as forgotPasswordSchemas from './forgotPasswordSchemas'
 
 export const types = `
 ${loginSchemas.types}
 ${validateTokenSchemas.types}
+${forgotPasswordSchemas.types}
 `
 
 export const Mutation = `
 ${loginSchemas.Mutation}
 ${validateTokenSchemas.Mutation}
+${forgotPasswordSchemas.Mutation}
 `
