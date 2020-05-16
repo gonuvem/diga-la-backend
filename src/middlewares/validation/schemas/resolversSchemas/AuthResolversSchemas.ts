a33930cb0b41579335bf968a73f62123d809b9b2
Edson Moita <moitaneto@hotmail.com>
2020-05-16 12:26:03 -0300

    Cria schema Joi que valida requisição à mutation forgotPassword.

--- src/middlewares/validation/schemas/resolversSchemas/AuthResolversSchemas.ts
+++ src/middlewares/validation/schemas/resolversSchemas/AuthResolversSchemas.ts
@@ -5,23 +5,29 @@ import { basicStringSchema } from '../baseSchemas'
 import {
   LoginParams,
   JoiSchemaMap,
-  ValidateTokenParams
+  ValidateTokenParams,
+  ForgotPasswordParams
 } from '../../../../types'
 
 const loginKeys: JoiSchemaMap<LoginParams> = {
   email: User.email.required(),
   password: User.password.required()
 }
-
 const login = Joi.object<LoginParams>().keys(loginKeys)
 
 const validateTokenKeys: JoiSchemaMap<ValidateTokenParams> = {
   token: basicStringSchema.required()
 }
-
 const validateToken = Joi.object().keys(validateTokenKeys)
+
+const forgotPasswordKeys: JoiSchemaMap<ForgotPasswordParams> = {
+  email: User.email.required()
+}
+const forgotPassword = Joi.object<ForgotPasswordParams>()
+  .keys(forgotPasswordKeys)
 
 export default {
   login,
-  validateToken
+  validateToken,
+  forgotPassword
 }
