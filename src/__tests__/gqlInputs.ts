c0fb00557e978024d50bc08ae1665543d759726c
Edson Moita <moitaneto@hotmail.com>
2020-05-18 14:56:45 -0300

    Adiciona função que cria input gql da mutation createClient para auxiliar nos testes.

--- src/__tests__/gqlInputs.ts
+++ src/__tests__/gqlInputs.ts
@@ -1,7 +1,13 @@
-import { UpdateOwnProfileInput } from '../types'
+import { UpdateOwnProfileInput, CreateClientInput } from '../types'
 
 export const createInputUpdateOwnProfile = (input: UpdateOwnProfileInput)
 : string => `{
   name: "${input.name}",
   email: "${input.email}"
+}`
+
+export const createInputCreateClient = (input: CreateClientInput): string => `{
+  name: "${input.name}",
+  email: "${input.email}",
+  password: "${input.password}"
 }`
