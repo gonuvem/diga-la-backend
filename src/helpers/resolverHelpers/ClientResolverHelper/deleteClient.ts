653120cc76845c77cf25acf15354f6df2daac961
Edson Moita <moitaneto@hotmail.com>
2020-05-18 16:06:29 -0300

    Cria função auxiliar que implementa lógica da mutation deleteClient.

--- src/helpers/resolverHelpers/ClientResolverHelper/deleteClient.ts
+++ src/helpers/resolverHelpers/ClientResolverHelper/deleteClient.ts
@@ -1,1 +1,20 @@
-export default (): void => {}
+import {
+  fetchOneClientWithUser,
+  checkClientInUse,
+  deleteOneClient
+} from '../../../services/models/ClientService'
+import { deleteOneUser } from '../../../services/models/UserService'
+import { UserDocument } from '../../../interfaces'
+
+export async function deleteClient ({ id }: { id: string }): Promise<object> {
+  const client = await fetchOneClientWithUser({ conditions: { _id: id } })
+
+  await checkClientInUse({ id: client._id })
+
+  await Promise.all([
+    deleteOneClient({ conditions: { _id: client._id } }),
+    deleteOneUser({ conditions: { _id: (client.user as UserDocument)._id } })
+  ])
+
+  return { }
+}
