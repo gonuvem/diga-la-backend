d292bffad711270a738163c107a2497e0c721595
Edson Moita <moitaneto@hotmail.com>
2020-05-18 15:43:22 -0300

    Refatora função para facilitar o reuso.

--- src/helpers/resolverHelpers/AuthResolverHelper/updateOwnProfile.ts
+++ src/helpers/resolverHelpers/AuthResolverHelper/updateOwnProfile.ts
@@ -1,48 +1,10 @@
-import {
-  UserDocument,
-  ClientDocument,
-  ClientInterface,
-  UserInterface
-} from '../../../interfaces'
+import { UserDocument, ClientDocument } from '../../../interfaces'
 import { UpdateOwnProfileInput } from '../../../types'
-import {
-  fetchOneClientWithUser,
-  updateOneClient
-} from '../../../services/models/ClientService'
-import { updateOneUser } from '../../../services/models/UserService'
-import { checkUserConflicts } from '../../UserHelper'
-
-const updateClientWithUser = async (client: ClientDocument,
-  clientData: Partial<ClientInterface>, userData: Partial<UserInterface>)
-  : Promise<ClientDocument> => {
-  const [clientUpdated, user] = await Promise.all([
-    updateOneClient({
-      conditions: { _id: client._id },
-      updateData: clientData
-    }),
-    updateOneUser({
-      conditions: { _id: (client.user as UserDocument)._id },
-      updateData: userData
-    })
-  ])
-
-  clientUpdated.user = user
-
-  return clientUpdated
-}
+import { fetchAndUpdateClientWithUser } from '../../ClientHelper'
 
 export async function updateOwnProfile (user: UserDocument,
   input: UpdateOwnProfileInput): Promise<{ client: ClientDocument }> {
-  const { ...userData } = input
+  const client = await fetchAndUpdateClientWithUser({ user: user._id }, input)
 
-  const client = await fetchOneClientWithUser({ conditions: { user: user._id } })
-
-  if (userData.email) {
-    const query = { _id: { $ne: (client.user as UserDocument)._id } }
-    await checkUserConflicts(userData, query)
-  }
-
-  const clientUpdated = await updateClientWithUser(client, { }, userData)
-
-  return { client: clientUpdated }
+  return { client }
 }
