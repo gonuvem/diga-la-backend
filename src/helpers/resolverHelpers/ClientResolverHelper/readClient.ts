f988a4bee0eaa5151bd7b158a0f8b2fb5fc8f3f6
Edson Moita <moitaneto@hotmail.com>
2020-05-19 12:02:49 -0300

    Cria função auxiliar que implementa lógica da query readClient.

--- src/helpers/resolverHelpers/ClientResolverHelper/readClient.ts
+++ src/helpers/resolverHelpers/ClientResolverHelper/readClient.ts
@@ -1,1 +1,9 @@
-export default (): void => {}
+import { ClientDocument } from '../../../interfaces'
+import { fetchOneClientWithUser } from '../../../services/models/ClientService'
+
+export async function readClient ({ id }: { id: string })
+: Promise<{ client: ClientDocument }> {
+  const client = await fetchOneClientWithUser({ conditions: { _id: id } })
+
+  return { client }
+}
