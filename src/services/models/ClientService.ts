382fe48b2faa49aee97d3cb62e5cbc793b123a9e
Edson Moita <moitaneto@hotmail.com>
2020-05-19 11:34:10 -0300

    Modifica a função que lista clientes para permitir uma ordenação pelos campos da referência usuário.

--- src/services/models/ClientService.ts
+++ src/services/models/ClientService.ts
@@ -7,13 +7,15 @@ import {
   updateOne,
   deleteOne,
   checkInUse,
-  listPaginatedWithPopulate
+  createLookupObj
 } from '../../utils/mongoose'
 import {
   CLIENTS_EMPTY_LIST,
   CLIENT_NOT_FOUND,
   CLIENT_IN_USE
 } from '../../middlewares/errorHandling/errors'
+import { isEmptyArray } from '../../utils/general'
+import { BaseListResult, ClientDocument } from '../../interfaces'
 
 const populate = 'user'
 
@@ -32,5 +34,22 @@ export const checkClientInUse = checkInUse(
   [{ Model: Form, fieldName: 'client' }], CLIENT_IN_USE
 )
 
-export const listClientsWithUserPaginated =
-listPaginatedWithPopulate(Client, CLIENTS_EMPTY_LIST, populate)
+export const listClientsWithUserPaginated = async (conditions: object,
+  projection: string, sort: string, page: number, perPage: number)
+  : Promise<BaseListResult<ClientDocument>> => {
+  const aggregation = Client.aggregate().match(conditions)
+    .lookup(createLookupObj('users', 'user')).unwind('user')
+
+  const [count, objects] = await Promise.all([
+    Client.aggregate(aggregation.pipeline()).count('total'),
+    aggregation.sort(sort).skip(page * perPage).limit(perPage)
+  ])
+
+  if (isEmptyArray(objects)) throw CLIENTS_EMPTY_LIST
+
+  const total = (count[0] && count[0].total) || 0
+
+  const pages = Math.ceil(total / perPage)
+
+  return { objects, total, pages }
+}
