171af2449cffe7d722aa3f9554eac6e0d1fa114b
Edson Moita <moitaneto@hotmail.com>
2020-05-19 12:01:35 -0300

    Cria testes para query readClient.

--- src/__tests__/resolvers/ClientResolvers/ClientResolvers.test.ts
+++ src/__tests__/resolvers/ClientResolvers/ClientResolvers.test.ts
@@ -2,6 +2,7 @@ import createClientTest from './createClientTest'
 import updateClientTest from './updateClientTest'
 import deleteClientTest from './deleteClientTest'
 import listClientsTest from './listClientsTest'
+import readClientTest from './readClientTest'
 
 describe('Test Client Resolvers', () => {
   describe('Test createClient', createClientTest)
@@ -11,4 +12,6 @@ describe('Test Client Resolvers', () => {
   describe('Test deleteClient', deleteClientTest)
 
   describe('Test listClients', listClientsTest)
+
+  describe('Test readClient', readClientTest)
 })
