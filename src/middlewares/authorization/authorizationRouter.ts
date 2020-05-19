756c0edcb5003df9524384f4082bee09a11fd46f
Edson Moita <moitaneto@hotmail.com>
2020-05-19 11:32:15 -0300

    Autoriza apenas o papel 'dev' a acessar a query listClients.

--- src/middlewares/authorization/authorizationRouter.ts
+++ src/middlewares/authorization/authorizationRouter.ts
@@ -10,5 +10,6 @@ export const gqlRouter: { [resolverName: string]: Role[] } = {
 
   createClient: [Role.Dev],
   updateClient: [Role.Dev],
-  deleteClient: [Role.Dev]
+  deleteClient: [Role.Dev],
+  listClients: [Role.Dev]
 }
