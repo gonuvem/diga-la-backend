0bac4a37e01aba5e462464aff37f0107c040a0ff
Edson Moita <moitaneto@hotmail.com>
2020-05-20 15:01:32 -0300

    Cria testes para mutation deleteOwnForm.

--- src/__tests__/resolvers/FormResolvers/FormResolvers.test.ts
+++ src/__tests__/resolvers/FormResolvers/FormResolvers.test.ts
@@ -1,8 +1,11 @@
 import createOwnFormTest from './createOwnFormTest'
 import updateOwnFormTest from './updateOwnFormTest'
+import deleteOwnFormTest from './deleteOwnFormTest'
 
 describe('Test Form Resolvers', () => {
   describe('Test createOwnForm', createOwnFormTest)
 
   describe('Test updateOwnForm', updateOwnFormTest)
+
+  describe('Test deleteOwnForm', deleteOwnFormTest)
 })
