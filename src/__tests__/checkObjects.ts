77bb6215a78ba4e925b37f2473bc174b8446beed
Edson Moita <moitaneto@hotmail.com>
2020-05-20 11:05:34 -0300

    Cria função que checa uma pesquisa para auxiliar nos testes.

--- src/__tests__/checkObjects.ts
+++ src/__tests__/checkObjects.ts
@@ -3,7 +3,9 @@ import {
   UserInterface,
   UserDocument,
   ClientInterface,
-  ClientDocument
+  ClientDocument,
+  FormDocument,
+  FormInterface
 } from '../interfaces'
 
 export const checkUser = (expected: Partial<UserInterface>,
@@ -21,4 +23,44 @@ export const checkUser = (expected: Partial<UserInterface>,
 export const checkClient = (expected: Partial<ClientInterface>,
   received: Partial<ClientDocument>): void => {
   checkUser(expected.user as UserInterface, received.user as UserInterface)
+}
+
+const checkFormConfig = (expected: Partial<FormInterface['config']>,
+  received: Partial<FormDocument['config']>): void => {
+  expect(received).toMatchObject({
+    name: expected.name,
+    description: expected.description,
+    beginDate: expected.beginDate.toISOString(),
+    endDate: expected.endDate.toISOString(),
+    hasLimitedResponses: expected.hasLimitedResponses,
+    maxResponses: expected.maxResponses,
+    isTotemMode: expected.isTotemMode,
+    canDisplayProgressBar: expected.canDisplayProgressBar,
+    progressBarType: expected.progressBarType,
+    canAllowMultipleSubmissions: expected.canAllowMultipleSubmissions
+  })
+}
+
+const checkFormStyle = (expected: Partial<FormInterface['style']>,
+  received: Partial<FormDocument['style']>): void => {
+  expect(received).toMatchObject({
+    background: expected.background,
+    logo: expected.logo,
+    headerText: expected.headerText,
+    hasLogoInHeader: expected.hasLogoInHeader,
+    headerBackground: expected.headerBackground,
+    footerText: expected.footerText,
+    footerBackground: expected.footerBackground
+  })
+}
+
+export const checkForm = (expected: Partial<FormInterface>,
+  received: Partial<FormDocument>): void => {
+  expect(received).toMatchObject({
+    isActive: expected.isActive
+  })
+  checkClient(expected.client as ClientInterface,
+    received.client as ClientDocument)
+  checkFormConfig(expected.config, received.config)
+  checkFormStyle(expected.style, received.style)
 }
