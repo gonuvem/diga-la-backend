4af9e810e9a77a85a017d27462ac413ab33445ab
Edson Moita <moitaneto@hotmail.com>
2020-05-26 18:05:20 -0300

    Cria testes para mutation updateOwnQuestion.

--- src/__tests__/resolvers/QuestionResolvers/QuestionResolvers.test.ts
+++ src/__tests__/resolvers/QuestionResolvers/QuestionResolvers.test.ts
@@ -1,5 +1,8 @@
 import createOwnQuestionTest from './createOwnQuestionTest'
+import updateOwnQuestionTest from './updateOwnQuestionTest'
 
 describe('Test Question Resolvers', () => {
   describe('Test createOwnQuestion', createOwnQuestionTest)
+
+  describe('Test updateOwnQuestion', updateOwnQuestionTest)
 })
