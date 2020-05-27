364d892a0bf43060f501ede8b7a94f820b3fb834
Edson Moita <moitaneto@hotmail.com>
2020-05-27 16:00:41 -0300

    Cria função que reposiciona questões após a exclusão de uma delas.

--- src/helpers/resolverHelpers/QuestionResolverHelper/deleteOwnQuestion.ts
+++ src/helpers/resolverHelpers/QuestionResolverHelper/deleteOwnQuestion.ts
@@ -1,22 +1,57 @@
-import { UserDocument } from '../../../interfaces'
+import { UserDocument, QuestionDocument } from '../../../interfaces'
 import {
   fetchOneQuestionWithFormAndType,
   checkQuestionInUse,
-  deleteOneQuestion
+  deleteOneQuestion,
+  fetchAllQuestions
 } from '../../../services/models/QuestionService'
 import { fetchOneClientWithUser } from '../../../services/models/ClientService'
+
+const getNextQuestionsFromSameFormPage = async (id: string, formPage: number,
+  outgoingPosition: number)
+: Promise<QuestionDocument[]> => {
+  return await fetchAllQuestions({
+    conditions: {
+      formPage,
+      _id: { $ne: id },
+      position: { $gt: outgoingPosition }
+    },
+    sort: 'position',
+    lean: false
+  })
+}
+
+const updateNextQuestionsPositions = async (position: number,
+  nextQuestions: QuestionDocument[])
+  : Promise<void> => {
+  for (const [i, question] of nextQuestions.entries()) {
+    question.position = position + i
+    await question.save()
+  }
+}
+
+const repositionQuestions = async (id: string, formPage: number,
+  outgoingPosition: number)
+: Promise<void> => {
+  const nextQuestions = await getNextQuestionsFromSameFormPage(id, formPage,
+    outgoingPosition)
+
+  await updateNextQuestionsPositions(outgoingPosition, nextQuestions)
+}
 
 export async function deleteOwnQuestion (user: UserDocument,
   { id }:{ id: string }): Promise<{}> {
   await fetchOneClientWithUser({ conditions: { user: user._id } })
 
-  const question = await fetchOneQuestionWithFormAndType({
+  const { _id, formPage, position } = await fetchOneQuestionWithFormAndType({
     conditions: { _id: id }
   })
 
-  await checkQuestionInUse({ id: question._id })
+  await checkQuestionInUse({ id: _id })
 
-  await deleteOneQuestion({ conditions: { _id: question._id } })
+  await repositionQuestions(_id, formPage, position)
+
+  await deleteOneQuestion({ conditions: { _id: _id } })
 
   return {}
 }
