75b462994de9be04a2ccc607421cddf4030f1df6
Edson Moita <moitaneto@hotmail.com>
2020-05-28 12:55:14 -0300

    Corrige fábrica de resposta com a mudança.

--- src/factories/models/Response.ts
+++ src/factories/models/Response.ts
@@ -1,10 +1,38 @@
-import { fakeId, fakeWord, fakeArray } from '../fakers'
+import {
+  fakeId,
+  fakeWord,
+  fakeArray,
+  fakeFutureDate,
+  fakeEmail,
+  fakePhoto,
+  fakeSentence,
+  fakeRandomInt,
+  fakePhone
+} from '../fakers'
 import { AnswerAndQuestion, ResponseInterface } from '../../interfaces'
 import { Fake } from '../../types'
 
+const fakeAnswer: AnswerAndQuestion['answer'] = {
+  checkBox: fakeArray(fakeWord, 3),
+  date: fakeArray(fakeFutureDate, 1),
+  dropDown: fakeArray(fakeWord, 1),
+  email: fakeEmail(),
+  imageChoice: fakeArray(fakeWord, 1),
+  link: fakePhoto(),
+  longText: fakeSentence(),
+  matrix: fakeArray(fakeWord, 3),
+  nps: fakeRandomInt({ min: 0, max: 9 })(),
+  number: fakeRandomInt({ min: 1, max: 100 })(),
+  phone: fakePhone(),
+  radioButton: fakeArray(fakeWord, 1),
+  shortText: fakeSentence(),
+  slider: fakeRandomInt({ min: 1, max: 100 })(),
+  sortList: fakeArray(fakeWord, 5)
+}
+
 const fakeAnswerAndQuestion: AnswerAndQuestion = {
   question: fakeId(),
-  answer: fakeWord()
+  answer: ((): AnswerAndQuestion['answer'] => fakeAnswer)()
 }
 
 export const Response: Fake<ResponseInterface> = {
