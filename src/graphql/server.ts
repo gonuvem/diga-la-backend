52d2c405a26135a466163df20561eaa1cb61e6e8
Edson Moita <moitaneto@hotmail.com>
2020-06-08 10:46:22 -0300

    Adiciona dataloader de respostas e questões ao contexto do servidor graphql.

--- src/graphql/server.ts
+++ src/graphql/server.ts
@@ -3,11 +3,20 @@ import { ApolloServer, IResolvers } from 'apollo-server-express'
 import typeDefs from './schemas'
 import resolvers from './resolvers'
 import { IS_NOT_PRODUCTION } from '../utils/constants'
+import {
+  getResponseAnswersAndQuestionsDataLoader
+} from '../helpers/resolverHelpers/ResponseResolverHelper'
 
 const gqlServer = new ApolloServer({
   typeDefs,
   resolvers: resolvers as IResolvers,
-  context: ({ req, res }): object => ({ req, res }),
+  context: ({ req, res }): object => ({
+    req,
+    res,
+    loaders: {
+      answersAndQuestionsLoader: getResponseAnswersAndQuestionsDataLoader()
+    }
+  }),
   playground: IS_NOT_PRODUCTION === true,
   introspection: IS_NOT_PRODUCTION === true
 })
