75a5310d9bc7c18e4e8b5fead611cc527a5d3ab3
Edson Moita <moitaneto@hotmail.com>
2020-07-14 16:52:47 -0300

    Cria função que cria array de questões.

--- src/services/models/QuestionService.ts
+++ src/services/models/QuestionService.ts
@@ -1,5 +1,5 @@
-import Question from '../../models/Question'
-import Response from '../../models/Response'
+import Question from '../../models/Question';
+import Response from '../../models/Response';
 import {
   createOne,
   fetchOne,
@@ -9,33 +9,37 @@ import {
   listPaginated,
   countTotal,
   fetchAll,
-  fetchOneWithoutError
-} from '../../utils/mongoose'
+  fetchOneWithoutError,
+  createMany,
+} from '../../utils/mongoose';
 import {
   QUESTIONS_EMPTY_LIST,
   QUESTION_NOT_FOUND,
-  QUESTION_IN_USE
-} from '../../middlewares/errorHandling/errors'
+  QUESTION_IN_USE,
+} from '../../middlewares/errorHandling/errors';
 
-export const createOneQuestion = createOne(Question)
+export const createOneQuestion = createOne(Question);
 
-export const fetchOneQuestion = fetchOne(Question, QUESTION_NOT_FOUND)
+export const createManyQuestions = createMany(Question);
 
-export const updateOneQuestion = updateOne(Question, QUESTION_NOT_FOUND)
+export const fetchOneQuestion = fetchOne(Question, QUESTION_NOT_FOUND);
 
-export const deleteOneQuestion = deleteOne(Question, QUESTION_NOT_FOUND)
+export const updateOneQuestion = updateOne(Question, QUESTION_NOT_FOUND);
+
+export const deleteOneQuestion = deleteOne(Question, QUESTION_NOT_FOUND);
 
 export const checkQuestionInUse = checkInUse(
-  [
-    { Model: Response, fieldName: 'answersAndQuestions.question' }
-  ], QUESTION_IN_USE
-)
+  [{ Model: Response, fieldName: 'answersAndQuestions.question' }],
+  QUESTION_IN_USE
+);
 
-export const listQuestionsPaginated = listPaginated(Question,
-  QUESTIONS_EMPTY_LIST)
+export const listQuestionsPaginated = listPaginated(
+  Question,
+  QUESTIONS_EMPTY_LIST
+);
 
-export const countQuestionsTotal = countTotal(Question)
+export const countQuestionsTotal = countTotal(Question);
 
-export const fetchAllQuestions = fetchAll(Question)
+export const fetchAllQuestions = fetchAll(Question);
 
-export const fetchOneQuestionWithoutError = fetchOneWithoutError(Question)
+export const fetchOneQuestionWithoutError = fetchOneWithoutError(Question);
