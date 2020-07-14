9f4843c675e6526e6dc01f276a099c40b3c529e7
Edson Moita <moitaneto@hotmail.com>
2020-07-14 16:53:43 -0300

    Cria tipagem para mutation createOwnQuestions.

--- src/types/params.ts
+++ src/types/params.ts
@@ -3,122 +3,134 @@ import {
   FormInterface,
   QuestionTypeInterface,
   QuestionInterface,
-  AnswerAndQuestion
-} from '../interfaces'
-import { ID } from '../types'
+  AnswerAndQuestion,
+} from '../interfaces';
+import { ID } from '../types';
 
 export type LoginParams = {
-  email: UserInterface['email'],
-  password: UserInterface['password']
-}
+  email: UserInterface['email'];
+  password: UserInterface['password'];
+};
 
 export type ValidateTokenParams = {
-  token: string
-}
+  token: string;
+};
 
 export type ForgotPasswordParams = {
-  email: UserInterface['email']
-}
+  email: UserInterface['email'];
+};
 
 export type RenewPasswordParams = {
-  email: UserInterface['email'],
-  password: UserInterface['password'],
-  code: UserInterface['renewPasswordCode']
-}
+  email: UserInterface['email'];
+  password: UserInterface['password'];
+  code: UserInterface['renewPasswordCode'];
+};
 
 export type UpdateOwnProfileInput = {
-  name: UserInterface['name'],
-  email: UserInterface['email']
-}
+  name: UserInterface['name'];
+  email: UserInterface['email'];
+};
 
 export type UpdateOwnPasswordParams = {
-  oldPassword: UserInterface['password'],
-  newPassword: UserInterface['password']
-}
+  oldPassword: UserInterface['password'];
+  newPassword: UserInterface['password'];
+};
 
 export type CreateClientInput = {
-  name: UserInterface['name'],
-  email: UserInterface['email'],
-  password: UserInterface['password'],
-}
+  name: UserInterface['name'];
+  email: UserInterface['email'];
+  password: UserInterface['password'];
+};
 
 export type UpdateClientInput = {
-  name: UserInterface['name'],
-  email: UserInterface['email']
-}
+  name: UserInterface['name'];
+  email: UserInterface['email'];
+};
 
 type ListParams<Filters = {}> = {
-  q?: string,
-  page?: number,
-  perPage?: number,
-  sort?: string,
-} & Filters
+  q?: string;
+  page?: number;
+  perPage?: number;
+  sort?: string;
+} & Filters;
 
-export type ListClientsParams = ListParams
+export type ListClientsParams = ListParams;
 
 export type CreateOwnFormInput = {
-  isActive: FormInterface['isActive'],
-  config: FormInterface['config'],
-  style?: FormInterface['style']
-}
+  isActive: FormInterface['isActive'];
+  config: FormInterface['config'];
+  style?: FormInterface['style'];
+};
 
 export type UpdateOwnFormInput = {
-  isActive?: FormInterface['isActive'],
-  config?: FormInterface['config'],
-  style?: FormInterface['style']
-}
+  isActive?: FormInterface['isActive'];
+  config?: FormInterface['config'];
+  style?: FormInterface['style'];
+};
 
-export type ListOwnFormsParams = ListParams
+export type ListOwnFormsParams = ListParams;
 
 export type CreateQuestionTypeInput = {
-  kind: QuestionTypeInterface['kind'],
-  alias: QuestionTypeInterface['alias'],
-  name: QuestionTypeInterface['name'],
-  cover: QuestionTypeInterface['cover'],
-  description: QuestionTypeInterface['description']
-}
+  kind: QuestionTypeInterface['kind'];
+  alias: QuestionTypeInterface['alias'];
+  name: QuestionTypeInterface['name'];
+  cover: QuestionTypeInterface['cover'];
+  description: QuestionTypeInterface['description'];
+};
 
 export type UpdateQuestionTypeInput = {
-  kind: QuestionTypeInterface['kind'],
-  alias: QuestionTypeInterface['alias'],
-  name: QuestionTypeInterface['name'],
-  cover: QuestionTypeInterface['cover'],
-  description: QuestionTypeInterface['description']
-}
+  kind: QuestionTypeInterface['kind'];
+  alias: QuestionTypeInterface['alias'];
+  name: QuestionTypeInterface['name'];
+  cover: QuestionTypeInterface['cover'];
+  description: QuestionTypeInterface['description'];
+};
 
 export type ListQuestionTypesParams = ListParams<{
-  kind?: QuestionTypeInterface['kind'],
-  alias?: QuestionTypeInterface['alias'],
-}>
+  kind?: QuestionTypeInterface['kind'];
+  alias?: QuestionTypeInterface['alias'];
+}>;
 
 export type CreateOwnQuestionInput = {
-  form: ID,
-  type: ID,
-  formPage: QuestionInterface['formPage'],
-  config: QuestionInterface['config']
-}
+  form: ID;
+  type: ID;
+  formPage: QuestionInterface['formPage'];
+  config: QuestionInterface['config'];
+};
 
 export type UpdateOwnQuestionInput = {
-  position?: QuestionInterface['position'],
-  config?: QuestionInterface['config']
-}
+  position?: QuestionInterface['position'];
+  config?: QuestionInterface['config'];
+};
 
 export type ListOwnQuestionsParams = ListParams<{
-  form: ID
-  formPage?: QuestionInterface['formPage']
-}>
+  form: ID;
+  formPage?: QuestionInterface['formPage'];
+}>;
 
 export type ListOwnResponsesParams = ListParams<{
-  form: ID,
-  question?: ID
-}>
+  form: ID;
+  question?: ID;
+}>;
 
 type AnswerAndQuestionInput = {
-  question: ID,
-  answer: AnswerAndQuestion['answer']
-}
+  question: ID;
+  answer: AnswerAndQuestion['answer'];
+};
 
 export type SubmitResponseInput = {
-  form: ID,
-  answersAndQuestions: AnswerAndQuestionInput[]
-}
+  form: ID;
+  answersAndQuestions: AnswerAndQuestionInput[];
+};
+
+export type QuestionInput = {
+  type: ID;
+  formPage: QuestionInterface['formPage'];
+  position: QuestionInterface['position'];
+  config: QuestionInterface['config'];
+};
+
+export type CreateOwnQuestionsInput = {
+  form: ID;
+  questions: QuestionInput[];
+};
