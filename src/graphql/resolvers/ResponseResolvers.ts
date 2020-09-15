63a41ea21960f2a84595cdeb51c1ea25b7dee6a8
Edson Moita <moitaneto@hotmail.com>
2020-09-15 11:43:26 -0300

    Cria resolver para query getChartsData.

--- src/graphql/resolvers/ResponseResolvers.ts
+++ src/graphql/resolvers/ResponseResolvers.ts
@@ -1,60 +1,90 @@
-import { wrapGqlAsyncFunc } from '../../middlewares/errorHandling/errorHelper'
-import validateGqlRequest from '../../middlewares/validation'
-import isGqlAuthenticated from '../../middlewares/authentication'
-import isGqlAuthorized from '../../middlewares/authorization'
-import * as ResponseResolverHelper from '../../helpers/resolverHelpers/ResponseResolverHelper'
+import { wrapGqlAsyncFunc } from '../../middlewares/errorHandling/errorHelper';
+import validateGqlRequest from '../../middlewares/validation';
+import isGqlAuthenticated from '../../middlewares/authentication';
+import isGqlAuthorized from '../../middlewares/authorization';
+import * as ResponseResolverHelper from '../../helpers/resolverHelpers/ResponseResolverHelper';
 import {
   MyContext,
   ListOwnResponsesParams,
   ListResponsesResponse,
   SubmitResponseInput,
-  AnswersAndQuestionsDataLoader
-} from '../../types'
-import { ResponseDocument, AnswerAndQuestion } from '../../interfaces'
+  AnswersAndQuestionsDataLoader,
+} from '../../types';
+import {
+  ResponseDocument,
+  AnswerAndQuestion,
+  ChartData,
+} from '../../interfaces';
 
-const listOwnResponses = (_parent: object, _args: object,
-  context: MyContext<ListOwnResponsesParams>)
-  : Promise<ListResponsesResponse> => {
-  return ResponseResolverHelper.listOwnResponses(context.user,
-    context.validData)
-}
+const listOwnResponses = (
+  _parent: object,
+  _args: object,
+  context: MyContext<ListOwnResponsesParams>
+): Promise<ListResponsesResponse> => {
+  return ResponseResolverHelper.listOwnResponses(
+    context.user,
+    context.validData
+  );
+};
 
-const readOwnResponse = (_parent: object, _args: object,
-  context: MyContext<{ id: string }>)
-  : Promise<{ response: ResponseDocument }> => {
-  return ResponseResolverHelper.readOwnResponse(context.user, context.validData)
-}
+const readOwnResponse = (
+  _parent: object,
+  _args: object,
+  context: MyContext<{ id: string }>
+): Promise<{ response: ResponseDocument }> => {
+  return ResponseResolverHelper.readOwnResponse(
+    context.user,
+    context.validData
+  );
+};
 
-const submitResponse = (_parent: object, _args: object,
-  context: MyContext<{ input: SubmitResponseInput }>)
-  : Promise<{ }> => {
-  return ResponseResolverHelper.submitResponse(context.validData.input)
-}
+const submitResponse = (
+  _parent: object,
+  _args: object,
+  context: MyContext<{ input: SubmitResponseInput }>
+): Promise<{}> => {
+  return ResponseResolverHelper.submitResponse(context.validData.input);
+};
 
-const getResponseAnswersAndQuestions = (response: ResponseDocument,
-  _args: object, context: MyContext<object, AnswersAndQuestionsDataLoader>)
-  : Promise<AnswerAndQuestion[]> => {
-  const { loaders } = context
+const getResponseAnswersAndQuestions = (
+  response: ResponseDocument,
+  _args: object,
+  context: MyContext<object, AnswersAndQuestionsDataLoader>
+): Promise<AnswerAndQuestion[]> => {
+  const { loaders } = context;
 
-  const { answersAndQuestionsLoader } = loaders
+  const { answersAndQuestionsLoader } = loaders;
 
-  return answersAndQuestionsLoader.load(response)
-}
+  return answersAndQuestionsLoader.load(response);
+};
+
+const getChartsData = (
+  _parent: object,
+  _args: object,
+  context: MyContext<{ id: string }>
+): Promise<{ data: ChartData[] }> => {
+  return ResponseResolverHelper.getChartsData(context.user, context.validData);
+};
 
 export const Query = {
-  listOwnResponses: wrapGqlAsyncFunc(isGqlAuthenticated(isGqlAuthorized(
-    validateGqlRequest(listOwnResponses)))),
-  readOwnResponse: wrapGqlAsyncFunc(isGqlAuthenticated(isGqlAuthorized(
-    validateGqlRequest(readOwnResponse))))
-}
+  listOwnResponses: wrapGqlAsyncFunc(
+    isGqlAuthenticated(isGqlAuthorized(validateGqlRequest(listOwnResponses)))
+  ),
+  readOwnResponse: wrapGqlAsyncFunc(
+    isGqlAuthenticated(isGqlAuthorized(validateGqlRequest(readOwnResponse)))
+  ),
+  getChartsData: wrapGqlAsyncFunc(
+    isGqlAuthenticated(isGqlAuthorized(validateGqlRequest(getChartsData)))
+  ),
+};
 
 export const Mutation = {
-  submitResponse: wrapGqlAsyncFunc(validateGqlRequest(submitResponse))
-}
+  submitResponse: wrapGqlAsyncFunc(validateGqlRequest(submitResponse)),
+};
 
 export const references = {
   Response: {
     form: ResponseResolverHelper.getResponseForm,
-    answersAndQuestions: getResponseAnswersAndQuestions
-  }
-}
+    answersAndQuestions: getResponseAnswersAndQuestions,
+  },
+};
