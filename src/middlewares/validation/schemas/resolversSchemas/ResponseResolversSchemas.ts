9c1e94bfd37e570e6f327c34bb9a872e4ae4155e
Edson Moita <moitaneto@hotmail.com>
2020-09-15 11:50:33 -0300

    Cria schema que valida requisições à query getChartsData.

--- src/middlewares/validation/schemas/resolversSchemas/ResponseResolversSchemas.ts
+++ src/middlewares/validation/schemas/resolversSchemas/ResponseResolversSchemas.ts
@@ -1,37 +1,35 @@
-import Joi from '@hapi/joi'
+import Joi from '@hapi/joi';
 
-import {
-  listSchema,
-  idSchema,
-  readSchema,
-  createSchema
-} from '../baseSchemas'
-import { Response } from '../models/Response'
+import { listSchema, idSchema, readSchema, createSchema } from '../baseSchemas';
+import { Response } from '../models/Response';
 import {
   JoiSchemaMap,
   ListOwnResponsesParams,
-  SubmitResponseInput
-} from '../../../../types'
+  SubmitResponseInput,
+} from '../../../../types';
 
-const sortFields = ['createdAt', '-createdAt']
-const defaultField = '-createdAt'
+const sortFields = ['createdAt', '-createdAt'];
+const defaultField = '-createdAt';
 const filters: JoiSchemaMap<ListOwnResponsesParams> = {
   form: Response.form.required(),
-  question: idSchema.optional()
-}
+  question: idSchema.optional(),
+};
 
-const listOwn = listSchema(sortFields, defaultField, filters)
+const listOwn = listSchema(sortFields, defaultField, filters);
 
-const readOwn = readSchema
+const readOwn = readSchema;
 
 const submitKeys: JoiSchemaMap<SubmitResponseInput> = {
   form: Response.form.required(),
-  answersAndQuestions: Response.answersAndQuestions.required()
-}
-const submit = createSchema(Joi.object().keys(submitKeys))
+  answersAndQuestions: Response.answersAndQuestions.required(),
+};
+const submit = createSchema(Joi.object().keys(submitKeys));
+
+const getChartsData = readSchema;
 
 export default {
   listOwnResponses: listOwn,
   readOwnResponse: readOwn,
-  submitResponse: submit
-}
+  submitResponse: submit,
+  getChartsData,
+};
