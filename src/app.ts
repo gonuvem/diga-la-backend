f6b81afc577dd6dab99ffa325bd36a3fd1a99e3d
Edson Moita <moitaneto@hotmail.com>
2021-05-03 11:13:20 -0300

    feat: :sparkles: add route /voyager to visually explore the API as an interactive graph

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,34 @@
-import express from 'express'
-import cors from 'cors'
-import compression from 'compression'
+import express from 'express';
+import cors from 'cors';
+import compression from 'compression';
+import { express as voyagerMiddleware } from 'graphql-voyager/middleware';
 
-import './env'
-import './sentry'
-import { gqlServer, gqlCors } from './graphql'
-import handleError from './middlewares/errorHandling'
-import logger from './middlewares/logging'
+import './env';
+import './sentry';
+import { gqlServer, gqlCors } from './graphql';
+import handleError from './middlewares/errorHandling';
+import logger from './middlewares/logging';
 
-const app = express()
+const app = express();
 
-app.use(logger)
+app.use(logger);
 
-app.use(cors())
+app.use(cors());
 
-app.use(compression())
+app.use(compression());
 
-app.use(express.json({ limit: '60mb' }))
+app.use(express.json({ limit: '60mb' }));
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (_, res) => { return res.json({ message: 'DIG API' }) })
+app.get('/', (_, res) => {
+  return res.json({ message: 'DIG API' });
+});
 
-gqlServer.applyMiddleware({ app, cors: gqlCors })
+app.use('/voyager', voyagerMiddleware({ endpointUrl: '/graphql' }));
 
-app.use(handleError)
+gqlServer.applyMiddleware({ app, cors: gqlCors });
 
-export default app
+app.use(handleError);
+
+export default app;
